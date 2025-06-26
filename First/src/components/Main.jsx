ceb95c54e24c644e90ef50afc75e44ce7306098a
Kuba <ksszewczykkuba@gmail.com>
2025-06-26 23:04:43 +0200

    First

---
+++ First/src/components/Main.jsx
@@ -0,0 +1,16 @@
+export default function () {
+	return (
+		<div className='main-wrap'>
+			<h1>Fun facts about React</h1>
+			<ul className='fact-list'>
+				<li className='list'>Was first released in 2013</li>
+				<li className='list'>Was originally created by Jordan Walke</li>
+				<li className='list'>Has well over 200K stars on GitHub</li>
+				<li className='list'>Is maintained by Meta</li>
+				<li className='list'>
+					Powers thousands of enterprise apps, including mobile apps
+				</li>
+			</ul>
+		</div>
+	);
+}
---
+++ First/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function () {
+	return (
+		<header>
+			<nav>
+				<img src='./src/img/react-logo.png' alt='react logo'></img>
+				<span className='nav-h1'>ReactFacts</span>
+			</nav>
+		</header>
+	);
+}
