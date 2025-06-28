6fed1dcde669d8e9f1207f120cc5663afc31f924
Kuba <ksszewczykkuba@gmail.com>
2025-06-29 01:49:00 +0200

    add secound

---
+++ Secound/Tak/src/App.jsx
@@ -0,0 +1,10 @@
+import Entry from './components/Entry';
+import Navbar from './components/Navbar';
+export default function () {
+	return (
+		<>
+			<Navbar />
+			<Entry />
+		</>
+	);
+}
---
+++ Secound/Tak/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function () {
+	return (
+		<header>
+			<nav>
+				<img src='./src/img/Globe.png' alt='globe'></img>
+				<h1>My travel journal.</h1>
+			</nav>
+		</header>
+	);
+}
