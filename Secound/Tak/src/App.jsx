b145b9fb6256c6d5a9311dff63f4cf2212ac3437
Kuba <ksszewczykkuba@gmail.com>
2025-06-30 01:44:08 +0200

    add props

--- Secound/Tak/src/App.jsx
+++ Secound/Tak/src/App.jsx
@@ -6,7 +6,30 @@ export default function () {
 		<>
 			<Navbar />
 			<Entry />
-			<Test />
+			<Test
+				img='./images/mr-whiskerson.png'
+				name='Mr. Whiskerson'
+				phone='(212) 555-1234'
+				email='mr.whiskaz@catnap.meow'
+			/>
+			<Test
+				img='./images/fluffykins.png'
+				name='Fluffykins'
+				phone='(212) 555-2345'
+				email='fluff@me.com'
+			/>
+			<Test
+				img='./images/felix.png'
+				name='Felix'
+				phone='(212) 555-4567'
+				email='thecat@hotmail.com'
+			/>
+			<Test
+				img='./images/pumpkin.png'
+				name='Pumpkin'
+				phone='(0800) CAT KING'
+				email='pumpkin@scrimba.com'
+			/>
 		</>
 	);
 }
--- Secound/Tak/src/components/Test.jsx
+++ Secound/Tak/src/components/Test.jsx
@@ -1,20 +1,16 @@
-export default function Test() {
-	const hours = new Date().getHours();
-	let timeOfDay;
-
-	if (hours < 12) {
-		timeOfDay = 'morning';
-	} else if (hours >= 12 && hours < 17) {
-		timeOfDay = 'afternoon';
-	} else if (hours < 21) {
-		timeOfDay = 'evening';
-	} else {
-		timeOfDay = 'night';
-	}
-
+export default function Test(props) {
 	return (
-		<>
-			<h1>Good {timeOfDay}</h1>
-		</>
+		<article className='contact-card'>
+			<img src={props.img} alt='Photo of Mr. Whiskerson' />
+			<h3>{props.name}</h3>
+			<div className='info-group'>
+				<img src='{props.img}' alt='phone icon' />
+				<p>{props.phone}</p>
+			</div>
+			<div className='info-group'>
+				<img src='./images/mail-icon.png' alt='mail icon' />
+				<p>{props.email}</p>
+			</div>
+		</article>
 	);
 }
