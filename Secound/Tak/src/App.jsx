9d94041b0955f5acdcf7ed7c49c681018587b7f7
Kuba <ksszewczykkuba@gmail.com>
2025-06-30 00:46:28 +0200

    tests

--- Secound/Tak/src/App.jsx
+++ Secound/Tak/src/App.jsx
@@ -1,10 +1,12 @@
 import Entry from './components/Entry';
 import Navbar from './components/Navbar';
+import Test from './components/Test';
 export default function () {
 	return (
 		<>
 			<Navbar />
 			<Entry />
+			<Test />
 		</>
 	);
 }
---
+++ Secound/Tak/src/components/Test.jsx
@@ -0,0 +1,20 @@
+export default function Test() {
+	const hours = new Date().getHours();
+	let timeOfDay;
+
+	if (hours < 12) {
+		timeOfDay = 'morning';
+	} else if (hours >= 12 && hours < 17) {
+		timeOfDay = 'afternoon';
+	} else if (hours < 21) {
+		timeOfDay = 'evening';
+	} else {
+		timeOfDay = 'night';
+	}
+
+	return (
+		<>
+			<h1>Good {timeOfDay}</h1>
+		</>
+	);
+}
