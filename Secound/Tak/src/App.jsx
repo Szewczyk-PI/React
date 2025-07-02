a83468bfbbfc70b468011be5397c973bc1959315
Kuba <ksszewczykkuba@gmail.com>
2025-07-02 13:40:44 +0200

    Import data from data.js to Entry component

--- Secound/Tak/src/App.jsx
+++ Secound/Tak/src/App.jsx
@@ -2,26 +2,28 @@ import Entry from './components/Entry';
 import Navbar from './components/Navbar';
 import jokesData from './components/jokesData';
 import Test from './components/Test';
+import data from './components/data';
 export default function () {
-	const jokesElements = jokesData.map((joke) => {
-		return <Test setup={joke.setup} punchline={joke.punchline} />;
+	const dataEntry = data.map((dat) => {
+		return (
+			<Entry
+				img={{
+					src: dat.img.src,
+					alt: dat.img.alt,
+				}}
+				title={dat.title}
+				country={dat.country}
+				googleMapsLink={dat.googleMapsLink}
+				dates={dat.dates}
+				text={dat.text}
+			/>
+		);
 	});
+
 	return (
 		<>
 			<Navbar />
-			{jokesElements}
-			<Entry
-				img={{
-					src: 'https://scrimba.com/links/travel-journal-japan-image-url',
-					alt: 'mount fuji',
-				}}
-				title='Mount Fuji'
-				country='Japan'
-				googleMapsLink='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu
-				'
-				dates='12 Jan, 2021 - 24 Jan, 2021'
-				text='Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.'
-			/>
+			<main className='container'>{dataEntry}</main>
 		</>
 	);
 }
