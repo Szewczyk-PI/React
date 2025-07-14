0fd6b60313f5dee2ad12e35586593e37a344b028
Kuba <ksszewczykkuba@gmail.com>
2025-07-15 00:20:36 +0200

    Adding Igredient

--- Third/Ai chef/src/components/form.jsx
+++ Third/Ai chef/src/components/form.jsx
@@ -1,13 +1,22 @@
 function Form() {
 	const ingredients = ['Chicken', 'Oregano', 'Tomatoes'];
 	const listIng = ingredients.map((ing) => <li key={ing}>{ing}</li>);
-	function consola() {
-		console.log('Form submited!');
+	function handleSubmit(event) {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		const newIngredient = formData.get('ingredient');
+		ingredients.push(newIngredient);
+		console.log(ingredients);
 	}
+
 	return (
 		<main>
-			<form onSubmit={consola()} className='add-ingredient-form'>
-				<input aria-label='Add ingredient' placeholder='e.g. oregano' />
+			<form onSubmit={handleSubmit} className='add-ingredient-form'>
+				<input
+					aria-label='Add ingredient'
+					placeholder='e.g. oregano'
+					name='ingredient'
+				/>
 				<button>Add ingredient</button>
 			</form>
 			<ul>{listIng}</ul>
