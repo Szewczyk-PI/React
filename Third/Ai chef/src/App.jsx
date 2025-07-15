aaa3cf4e5944447dbbc90f507e57e875d7c69071
Kuba <ksszewczykkuba@gmail.com>
2025-07-15 22:02:17 +0200

    useState

--- Third/Ai chef/src/App.jsx
+++ Third/Ai chef/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 		<>
 			<Header />
 			<Form />
-			{/* <Test /> */}
+			<Test />
 		</>
 	);
 }
--- Third/Ai chef/src/components/test.jsx
+++ Third/Ai chef/src/components/test.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 function Test() {
+	let [result, setResult] = useState('Yes');
+
+	console.log(result);
+
+	function handleClick() {
+		setResult('Hell Yes');
+	}
+
 	return (
 		<>
-			<h1>Test</h1>
+			<h1 className='title'>Is state important to know?</h1>
+			<button onClick={handleClick} className='value'>
+				{result}
+			</button>
 		</>
 	);
 }
