7566a05881d5add30ca74993ccf1018208bb5f55
Kuba <ksszewczykkuba@gmail.com>
2025-07-20 00:31:15 +0200

    update objects in test.jsc

--- Third/Ai chef/src/components/test.jsx
+++ Third/Ai chef/src/components/test.jsx
@@ -1,44 +1,56 @@
-import { useState } from 'react';
-// import './test.css';
+import React from 'react';
+import avatar from '../images/user.png';
+import starFilled from '../images/star-filled.png';
+import starEmpty from '../images/star-empty.png';
+import './test.css';
 
-function Test() {
-	/**
-	 * Challenge: Convert the code below to use an array
-	 * held in state instead of a local variable. Initialize
-	 * the state array as an empty array
-	 *
-	 * Don't worry about fixing `addFavoriteThing` quite yet.
-	 */
-	const [myFavoriteThings, setMyFavoriteThings] = useState([]);
-	const allFavoriteThings = [
-		'💦🌹',
-		'😺',
-		'💡🫖',
-		'🔥🧤',
-		'🟤🎁',
-		'🐴',
-		'🍎🥧',
-		'🚪🔔',
-		'🛷🔔',
-		'🥩🍝',
-	];
-	const thingsElements = myFavoriteThings.map((thing) => (
-		<p key={thing}>{thing}</p>
-	));
+export default function Test() {
+	const [contact, setContact] = React.useState({
+		firstName: 'John',
+		lastName: 'Doe',
+		phone: '+1 (212) 555-1212',
+		email: 'itsmyrealname@example.com',
+		isFavorite: false,
+	});
 
-	function addFavoriteThing() {
-		setMyFavoriteThings((prevFavThings) => [
-			...prevFavThings,
-			allFavoriteThings[prevFavThings.length],
-		]);
+	let starIcon = contact.isFavorite ? starFilled : starEmpty;
+	let ariaCheck = contact.isFavorite
+		? 'Remove from favorites'
+		: 'Add to favorites';
+	let altCheck = contact.isFavorite ? 'filled star icon' : 'unfilled star icon';
+	function toggleFavorite() {
+		setContact((prevContact) => {
+			return {
+				...prevContact,
+				isFavorite: !prevContact.isFavorite,
+			};
+		});
 	}
 
 	return (
 		<main>
-			<button onClick={addFavoriteThing}>Add item</button>
-			<section aria-live='polite'>{thingsElements}</section>
+			<article className='card'>
+				<img
+					src={avatar}
+					className='avatar'
+					alt='User profile picture of John Doe'
+				/>
+				<div className='info'>
+					<button
+						onClick={toggleFavorite}
+						aria-pressed={contact.isFavorite}
+						aria-label={ariaCheck}
+						className='favorite-button'
+					>
+						<img src={starIcon} alt={altCheck} className='favorite' />
+					</button>
+					<h2 className='name'>
+						{contact.firstName} {contact.lastName}
+					</h2>
+					<p className='contact'>{contact.phone}</p>
+					<p className='contact'>{contact.email}</p>
+				</div>
+			</article>
 		</main>
 	);
 }
-
-export default Test;
