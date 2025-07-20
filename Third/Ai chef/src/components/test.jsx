e470294128b032b5eff51bd106111eb6a2056001
Kuba <ksszewczykkuba@gmail.com>
2025-07-20 19:40:54 +0200

    Form in React 2

--- Third/Ai chef/src/components/test.jsx
+++ Third/Ai chef/src/components/test.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './test.css';
 export default function Test() {
+	function handleSubmit(formData) {
+		const email = formData.get('email');
+		const pass = formData.get('password');
+		console.log(email);
+		console.log(pass);
+	}
+
 	return (
 		<section>
 			<h1>Signup form</h1>
-			<form>
-				<label>
-					Email:
-					<input type='email' name='email' placeholder='joe@schmoe.com' />
-				</label>
+			<form action={handleSubmit}>
+				<label htmlFor='email'>Email:</label>
+				<input
+					id='email'
+					type='email'
+					name='email'
+					placeholder='joe@schmoe.com'
+				/>
+
 				<br />
 				<label htmlFor='password'>Password:</label>
 				<input id='password' type='password' name='password'></input>
+				<br />
+				<button>Submit</button>
 			</form>
 		</section>
 	);
