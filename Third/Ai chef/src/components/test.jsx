acaa7f18eec7363792b6b70ad0f83f8303ee8b5f
Kuba <ksszewczykkuba@gmail.com>
2025-07-20 19:09:20 +0200

    Form in React 1

--- Third/Ai chef/src/components/test.jsx
+++ Third/Ai chef/src/components/test.jsx
@@ -1,56 +1,19 @@
 import React from 'react';
-import avatar from '../images/user.png';
-import starFilled from '../images/star-filled.png';
-import starEmpty from '../images/star-empty.png';
+import ReactDOM from 'react-dom/client';
 import './test.css';
-
 export default function Test() {
-	const [contact, setContact] = React.useState({
-		firstName: 'John',
-		lastName: 'Doe',
-		phone: '+1 (212) 555-1212',
-		email: 'itsmyrealname@example.com',
-		isFavorite: false,
-	});
-
-	let starIcon = contact.isFavorite ? starFilled : starEmpty;
-	let ariaCheck = contact.isFavorite
-		? 'Remove from favorites'
-		: 'Add to favorites';
-	let altCheck = contact.isFavorite ? 'filled star icon' : 'unfilled star icon';
-	function toggleFavorite() {
-		setContact((prevContact) => {
-			return {
-				...prevContact,
-				isFavorite: !prevContact.isFavorite,
-			};
-		});
-	}
-
 	return (
-		<main>
-			<article className='card'>
-				<img
-					src={avatar}
-					className='avatar'
-					alt='User profile picture of John Doe'
-				/>
-				<div className='info'>
-					<button
-						onClick={toggleFavorite}
-						aria-pressed={contact.isFavorite}
-						aria-label={ariaCheck}
-						className='favorite-button'
-					>
-						<img src={starIcon} alt={altCheck} className='favorite' />
-					</button>
-					<h2 className='name'>
-						{contact.firstName} {contact.lastName}
-					</h2>
-					<p className='contact'>{contact.phone}</p>
-					<p className='contact'>{contact.email}</p>
-				</div>
-			</article>
-		</main>
+		<section>
+			<h1>Signup form</h1>
+			<form>
+				<label>
+					Email:
+					<input type='email' name='email' placeholder='joe@schmoe.com' />
+				</label>
+				<br />
+				<label htmlFor='password'>Password:</label>
+				<input id='password' type='password' name='password'></input>
+			</form>
+		</section>
 	);
 }
