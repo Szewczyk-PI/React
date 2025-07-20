87cd078042d4a14127e64d7d7623208049319221
Kuba <ksszewczykkuba@gmail.com>
2025-07-20 23:42:59 +0200

    Form in React 3

--- Third/Ai chef/src/components/test.jsx
+++ Third/Ai chef/src/components/test.jsx
@@ -1,30 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './test.css';
+import './test.css';
+
 export default function Test() {
-	function handleSubmit(formData) {
+	function signUp(formData) {
 		const email = formData.get('email');
-		const pass = formData.get('password');
-		console.log(email);
-		console.log(pass);
+		const password = formData.get('password');
+		const check = formData.get('employmentStatus');
+		const check2 = formData.getAll('dietaryRestrictions');
+		console.log(check2);
 	}
 
 	return (
 		<section>
 			<h1>Signup form</h1>
-			<form action={handleSubmit}>
+			<form action={signUp}>
 				<label htmlFor='email'>Email:</label>
 				<input
 					id='email'
+					defaultValue='joe@schmoe.com'
 					type='email'
 					name='email'
 					placeholder='joe@schmoe.com'
 				/>
 
-				<br />
 				<label htmlFor='password'>Password:</label>
-				<input id='password' type='password' name='password'></input>
-				<br />
+				<input
+					id='password'
+					defaultValue='password123'
+					type='password'
+					name='password'
+				/>
+
+				<label htmlFor='description'>Description:</label>
+				<textarea
+					id='description'
+					name='description'
+					defaultValue='This is a description'
+				></textarea>
+
+				<fieldset>
+					<legend>Employment Status:</legend>
+					<label>
+						<input type='radio' name='employmentStatus' value='Unemployed' />
+						Unemployed
+					</label>
+					<label>
+						<input type='radio' name='employmentStatus' value='Part-time' />
+						Part-time
+					</label>
+					<label>
+						<input
+							type='radio'
+							name='employmentStatus'
+							value='Full-time'
+							defaultChecked={true}
+						/>
+						Full-time
+					</label>
+				</fieldset>
+
+				<fieldset>
+					<legend>Dietary restrictions:</legend>
+					<label>
+						<input type='checkbox' name='dietaryRestrictions' value='kosher' />
+						Kosher
+					</label>
+					<label>
+						<input type='checkbox' name='dietaryRestrictions' value='vegan' />
+						Vegan
+					</label>
+					<label>
+						<input
+							type='checkbox'
+							name='dietaryRestrictions'
+							defaultChecked={true}
+							value='gluten-free'
+						/>
+						Gluten-free
+					</label>
+				</fieldset>
+
 				<button>Submit</button>
 			</form>
 		</section>
