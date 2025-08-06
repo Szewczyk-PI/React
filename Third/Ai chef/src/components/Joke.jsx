88dc2a9b3a84921efcb6b902222711c19341713f
Kuba <ksszewczykkuba@gmail.com>
2025-08-06 19:03:41 +0200

    First optional &&

--- Third/Ai chef/src/components/Joke.jsx
+++ Third/Ai chef/src/components/Joke.jsx
@@ -1,22 +1,18 @@
 import { useState } from 'react';
 
 export default function Joke(props) {
-	/**
-	 * Challenge:
-	 * - Create state `isShown` (boolean, default to `false`)
-	 * - Add a button that toggles the value back and forth
-	 */
 	const [isShown, setIsShown] = useState(false);
 
 	function toggleShown() {
 		setIsShown((prevShown) => !prevShown);
 	}
-	console.log();
+
 	return (
 		<div>
 			{props.setup && <h3>{props.setup}</h3>}
 			<button onClick={toggleShown}>Show punchline</button>
-			<p>{props.punchline}</p>
+			{isShown && <p>{props.punchline}</p>}
+
 			<hr />
 		</div>
 	);
